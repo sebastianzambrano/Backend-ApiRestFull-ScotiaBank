6e9f21b1eeda2e9cba08fc0267cc5f23e9590a7a
sebastianzambrano <jszambranop@gmail.com>
2024-04-07 19:06:48 -0500

    modificacion estructura backend e integracion con  base de dato mongo db

--- src/main/java/com/application/restfull/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.restfull.repository;
-
-import com.application.restfull.entities.Client;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends CrudRepository<Client,Long> {
-}
